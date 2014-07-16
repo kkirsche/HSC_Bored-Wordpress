99803ea142406ca6ae9afd7d98de36551482b417
Kevin Kirsche <kev.kirsche@gmail.com>
2014-07-16 15:29:58 -0400

    Include index.php for themes / plugins, etc.

---
+++ wp-content/themes/hscBoredAutumn/index.php
@@ -0,0 +1,201 @@
+<?php get_header(); ?>
+<div class="row">
+    <h3 style="margin-top: 0;">FEATURED EVENTS</h3>
+</div>
+<div class="row">
+    <div class="jumbotron">
+        <div class="col-lg-9 col-md-9 col-sm-9" id="inJumbotronSlider">
+            <div id="mainCarousel" class="carousel slide" data-ride="carousel">
+                <!--Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#mainCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#mainCarousel" data-slide-to="1"></li>
+                    <li data-target="#mainCarousel" data-slide-to="2"></li>
+                </ol>
+
+                <!--Wrapper for slides-->
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img src="http://localhost:8888/wordpress/HSC_Bored-Wordpress/wp-content/themes/hscBoredAutumn/img/slider/img1.jpg" alt="Image 1">
+                    </div>
+                    <div class="item">
+                        <img src="http://localhost:8888/wordpress/HSC_Bored-Wordpress/wp-content/themes/hscBoredAutumn/img/slider/img2.jpg" alt="Image 2">
+                    </div>
+                    <div class="item">
+                        <img src="http://localhost:8888/wordpress/HSC_Bored-Wordpress/wp-content/themes/hscBoredAutumn/img/slider/img3.jpg" alt="Image 3">
+                    </div>
+                </div>
+
+                <!--Controls-->
+                <a class="left carousel-control" href="#mainCarousel" role="button" data-slide="prev">
+                    <span class="fa fa-chevron-left fa-2x"></span>
+                </a>
+                <a class="right carousel-control" href="#mainCarousel" role="button" data-slide="next">
+                    <span class="fa fa-chevron-right fa-2x"></span>
+                </a>
+            </div><!--End Carousel-->
+        </div><!--End #inJumbotronSlider-->
+        <div class="col-lg-3 col-md-3 col-sm-3">
+            <div class="sliderData" id="sliderData1">
+                <h2>LAMBORGHINI CAR SHOW</h2>
+                <p>
+                    <strong>DATE:</strong> Thursday, June 27th<br />
+                    <strong>LOCATION:</strong> Somewhere, VA<br />
+                    <strong>TIME:</strong> 6 &ndash; 11 PM
+                </p>
+                <a href="#" title="Read More About..." class="readMoreButton">
+                    READ MORE &raquo;
+                </a>
+            </div><!--End #sliderData1-->
+            <div class="sliderData" id="sliderData2" style="display: none;">
+                <h2>LAMBORGHINI CAR SHOW 2</h2>
+                <p>
+                    <strong>DATE:</strong> Thursday, June 27th<br />
+                    <strong>LOCATION:</strong> Somewhere, VA<br />
+                    <strong>TIME:</strong> 6 &ndash; 11 PM
+                </p>
+                <a href="#" title="Read More About..." class="readMoreButton">
+                    READ MORE &raquo;
+                </a>
+            </div><!--End #sliderData2-->
+            <div class="sliderData" id="sliderData3" style="display: none;">
+                <h2>LAMBORGHINI CAR SHOW 3</h2>
+                <p>
+                    <strong>DATE:</strong> Thursday, June 27th<br />
+                    <strong>LOCATION:</strong> Somewhere, VA<br />
+                    <strong>TIME:</strong> 6 &ndash; 11 PM
+                </p>
+                <a href="#" title="Read More About..." class="readMoreButton">
+                    READ MORE &raquo;
+                </a>
+            </div><!--End #sliderData3-->
+        </div>
+    </div><!--End Jumbotron-->
+</div><!--End Row-->
+<div class="row">
+    <div class="col-lg-9"> <!--Begin Main Content Area-->
+        <h3 class="eventSectionHeading">UPCOMING ON-CAMPUS EVENTS <small class="textColorMediumOrange">/<br class="visible-xs"> <a href="#" class="textColorMediumOrange">view all &raquo;</a></small></h3>
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-6">
+                <div class="eventWrapper">
+                    <img src="http://localhost:8888/wordpress/HSC_Bored-Wordpress/wp-content/themes/hscBoredAutumn/img/event/img1.jpg" alt="Event 1 Image" class="eventImage img-responsive" title="Event 1">
+                    <div class="eventContent">
+                        <h4>
+                            EDC LAS VEGAS
+                        </h4>
+                        <p>
+                            <strong>DATE:</strong>Thursday, June 27th<br />
+                            <strong>LOCATION:</strong> Somewhere, VA<br />
+                            <strong>TIME:</strong> 6 &ndash; 11 PM
+                        </p>
+                        <a href="#" title="Read More About..." class="smallReadMoreButton">
+                            READ MORE &raquo;
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-6">
+                <div class="eventWrapper">
+                    <img src="http://localhost:8888/wordpress/HSC_Bored-Wordpress/wp-content/themes/hscBoredAutumn/img/event/img1.jpg" alt="Event 2 Image" class="eventImage img-responsive" title="Event 1">
+                    <div class="eventContent">
+                        <h4>
+                            EDC LAS VEGAS
+                        </h4>
+                        <p>
+                            <strong>DATE:</strong>Thursday, June 27th<br />
+                            <strong>LOCATION:</strong> Somewhere, VA<br />
+                            <strong>TIME:</strong> 6 &ndash; 11 PM
+                        </p>
+                        <a href="#" title="Read More About..." class="smallReadMoreButton">
+                            READ MORE &raquo;
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-6">
+                <div class="eventWrapper">
+                    <img src="http://localhost:8888/wordpress/HSC_Bored-Wordpress/wp-content/themes/hscBoredAutumn/img/event/img1.jpg" alt="Event 3 Image" class="eventImage img-responsive" title="Event 1">
+                    <div class="eventContent">
+                        <h4>
+                            EDC LAS VEGAS
+                        </h4>
+                        <p>
+                            <strong>DATE:</strong>Thursday, June 27th<br />
+                            <strong>LOCATION:</strong> Somewhere, VA<br />
+                            <strong>TIME:</strong> 6 &ndash; 11 PM
+                        </p>
+                        <a href="#" title="Read More About..." class="smallReadMoreButton">
+                            READ MORE &raquo;
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br />
+        <h3>UPCOMING OFF-CAMPUS EVENTS <small class="textColorMediumOrange">/<br class="visible-xs"> <a href="#" class="textColorMediumOrange">view all &raquo;</a></small></h3>
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-6">
+                <div class="eventWrapper">
+                    <img src="http://localhost:8888/wordpress/HSC_Bored-Wordpress/wp-content/themes/hscBoredAutumn/img/event/img1.jpg" alt="Event 1 Image" class="eventImage" title="Event 1">
+                    <div class="eventContent">
+                        <h4>
+                            EDC LAS VEGAS
+                        </h4>
+                        <p>
+                            <strong>DATE:</strong>Thursday, June 27th<br />
+                            <strong>LOCATION:</strong> Somewhere, VA<br />
+                            <strong>TIME:</strong> 6 &ndash; 11 PM
+                        </p>
+                        <a href="#" title="Read More About..." class="smallReadMoreButton">
+                            READ MORE &raquo;
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-6">
+                <div class="eventWrapper">
+                    <img src="http://localhost:8888/wordpress/HSC_Bored-Wordpress/wp-content/themes/hscBoredAutumn/img/event/img1.jpg" alt="Event 2 Image" class="eventImage" title="Event 1">
+                    <div class="eventContent">
+                        <h4>
+                            EDC LAS VEGAS
+                        </h4>
+                        <p>
+                            <strong>DATE:</strong>Thursday, June 27th<br />
+                            <strong>LOCATION:</strong> Somewhere, VA<br />
+                            <strong>TIME:</strong> 6 &ndash; 11 PM
+                        </p>
+                        <a href="#" title="Read More About..." class="smallReadMoreButton">
+                            READ MORE &raquo;
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-6">
+                <div class="eventWrapper">
+                    <img src="http://localhost:8888/wordpress/HSC_Bored-Wordpress/wp-content/themes/hscBoredAutumn/img/event/img1.jpg" alt="Event 3 Image" class="eventImage" title="Event 1">
+                    <div class="eventContent">
+                        <h4>
+                            EDC LAS VEGAS
+                        </h4>
+                        <p>
+                            <strong>DATE:</strong>Thursday, June 27th<br />
+                            <strong>LOCATION:</strong> Somewhere, VA<br />
+                            <strong>TIME:</strong> 6 &ndash; 11 PM
+                        </p>
+                        <a href="#" title="Read More About..." class="smallReadMoreButton">
+                            READ MORE &raquo;
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!--End Primary Event Content Area-->
+    <div class="col-lg-3">
+        <div class="eventWrapper" id="socialMedia">
+            <?php get_sidebar(); ?>
+        </div>
+    </div><!--End Social Media Area-->
+</div><!--End Row-->
+
+<?php wp_footer(); // Crucial footer hook! ?>
+<?php get_footer(); ?>
